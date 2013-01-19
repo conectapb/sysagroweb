bf8d30f7e35595d2323741588565d4a7a0055faf
kadisley <kadisley@gmail.com>
2013-01-19 15:06:51 +0000

    subindo usuario show

--- application/controllers/UsuarioController.php
+++ application/controllers/UsuarioController.php
@@ -22,93 +22,15 @@ class UsuarioController extends Zend_Controller_Action{
         $this->view->usuario = $paginator;
     }
 
-     public function addAction(){
+    public function showAction(){
+        //$this->assecoAction();
         $this->_helper->layout->setLayout('administrator');
-        $form = new Application_Form_Usuario();
-        $this->view->form = $form;
-        if ($this->getRequest()->isPost()) {
-            $formData = $this->getRequest()->getPost();
-            if ($form->isValid($formData))
-            {
-                $username      = $form->getValue('username');
-                $password      = $form->getValue('password');
-		$salt          = $form->getValue('password');
-                $role          = $form->getValue('role');
-                $date_created  = $form->getValue('date_created');
-                $this->usuario->comandInsert(
-                                      $username
-                                    , $password
-                                    , $salt
-                                    , $role
-                                    , $date_created );
-
-                    if($this->usuario)
-                    {
-                       $this->_helper->flashMessenger->addMessage(array('successo'=>'Registro Gravado com sucesso'));
-                       $this->_helper->redirector('sucesso');
-                    }
-            } else {
-                $form->populate($formData);
+        $id = $this->getRequest()->getParam('id');
+            if($id > 0){
+                //$usuarios = $this->usuario->find($id)->current();
+                $this->view->usuario = $this->usuario->find($id)->current();
             }
-        }
-     }
-     public function editarAction(){
-        $this->_helper->layout->setLayout('administrator');
-        $form = new Application_Form_Usuario();
-        $this->view->form = $form;
-        if ($this->getRequest()->isPost())
-        {
-            $formData = $this->getRequest()->getPost();
-            if ($form->isValid($formData))
-            {
-                $id            = (int) $form->getValue('id');
-                $username      = $form->getValue('username');
-                $password      = $form->getValue('password');
-		$salt          = $form->getValue('password');
-                $role          = $form->getValue('role');
-                $date_created  = $form->getValue('date_created');
-                $this->usuario->comandUpdates(
-                                            $id
-                                            , $username
-                                            , $password
-                                            , $salt
-                                            , $role
-                                            , $date_created );
-               if($this->usuario)
-                    {
-                       $this->_helper->flashMessenger->addMessage(
-                               array('successo'=>'Registro alterado com sucesso'));
-                       $this->_helper->redirector('sucesso');
-                    }
-            } else {
-                $form->populate($formData);
-            }
-        } else {
-            $id = $this->_getParam('id', 0);
-            if ($id > 0)
-            {
-                $form->populate($this->usuario->getId($id));
-            }
-        }
-    }
-     public function excluirAction() {
-       $this->_helper->layout->setLayout('administrator');
-       // Recupera o id
-       $id = $this->_request->getParam("id", 0);
-       // Verifica se existe id
-        if($id > 0) {
-           // Cria o where
-           // Remove o registro
-           $this->usuario->comandDelete(array("id = ?" => $id));
-        }
-        // Redireciona
-        $this->_helper->flashMessenger->addMessage(array('successo'=>'Registro excluido com sucesso'));
-        $this->_helper->redirector('sucesso');
-    }
-     public function sucessoAction(){
-        $this->_helper->layout->setLayout('administrator');
-        $dados = "sucesso";
-	$this->view->assign("dados", $dados);
+            else $this->view->message = 'registro não existe';
     }
 
 }?>
