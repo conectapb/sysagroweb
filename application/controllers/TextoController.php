c0149b16534f75a1ea5ab819606061bd75df984a
axelpatton <axelpatton@gmail.com>
2013-02-01 19:03:15 +0000

    atualização

--- application/controllers/TextoController.php
+++ application/controllers/TextoController.php
@@ -12,46 +12,39 @@
 
 class TextoController extends Zend_Controller_Action{
 
-
+   private $_file_texto = '../public/upload/texto/';
+    
     public function init(){
 	   $this->texto = new Application_Model_DbTable_Texto(); // DbTable
     }
     
-     public function indexAction(){
-        //$this->_helper->layout->setLayout('administrator');
-        $model = new Application_Model_Texto();
-        $dados = $model->select();
-        $this->view->assign("dados", $dados);
-    }
-
     public function textoAction(){
-        try{
-            //$id = $this->_getParam('rotulo', 0);
-            $id = $this->getRequest()->getParam('rotulo');
-            if (!empty($id))
-            {
-                $tex = new Application_Model_Texto();
-                $dados = $tex->select("rotulo = '$id' ", "id", 1);
-                //$dados = $tex->select("rotulo LIKE '%$id'", "id", 1);
-                $this->view->assign("dados", $dados);
-            }
-        } catch (Zend_Db_Exception $exc) {
-
-            echo $exc->getTraceAsString();
-        }
-     }
-
-
-    // region administador
-    public function listAction(){
-
+    	try{
+    		//$id = $this->_getParam('rotulo', 0);
+    		$id = $this->getRequest()->getParam('rotulo');
+    		if (!empty($id))
+    		{
+    			$tex = new Application_Model_Texto();
+    			$dados = $tex->select("rotulo = '$id' ", "id", 1);
+    			//$dados = $tex->select("rotulo LIKE '%$id'", "id", 1);
+    			$this->view->assign("dados", $dados);
+    		}
+    	} catch (Zend_Db_Exception $exc) {
+    
+    		echo $exc->getTraceAsString();
+    	}
+    }
+    
+    
+	// region administador
+    public function indexAction(){
+        
+        $this->assecoAction();
+        
         $this->_helper->layout->setLayout('administrator');
         // Página que a paginação irá iniciar
         $pagina = intval($this->_getParam('pagina', 1));
-
-        //$categoria = new Application_Model_DbTable_Categoria();
-        //$dados = $this->Deejay_DbTable->fetchAll();
-        $dados = $this->texto->fetchAll();
+        $dados  = $this->texto->fetchAll();
         $paginator = Zend_Paginator::factory($dados);
         // Seta a quantidade de registros por página
         $paginator->setItemCountPerPage(10);
@@ -60,35 +53,54 @@ class TextoController extends Zend_Controller_Action{
         // Seta a página atual
         $paginator->setCurrentPageNumber($pagina);
         // Passa o paginator para a view
-        $this->view->deejay = $paginator;
-    }
-    public function newAction(){
-	   $this->assecoAction();
+        $this->view->texto = $paginator;
+    }  
+    public function addAction(){
+        
+        $this->assecoAction();
+        
         $this->_helper->layout->setLayout('administrator');
-        $form = new Application_Form_Deejay();
+        $form = new Application_Form_Texto();
         $this->view->form = $form;
         if ($this->getRequest()->isPost() and $form->isValid($_POST)  ) {
             $formData = $this->getRequest()->getPost();
             if ($form->isValid($formData))
             {
-                $nome 	       = $form->getValue('nome');
-                $endereco      = $form->getValue('endereco');
-                $bibliografia  = $form->getValue('bibliografia');
-                $bairro        = $form->getValue('bairro');
-                $email         = $form->getValue('email');
-                $telefone      = $form->getValue('telefone');
-                $celular       = $form->getValue('celular'); 
-                $this->texto->add($nome
-                        , $endereco
-                        , $bibliografia
-                        , $bairro
-                        , $email
-                        , $telefone
-                        , $celular
-                        , $nameFile);
+                $rotulo      = $form->getValue('rotulo');
+                $titulo      = $form->getValue('titulo');
+                $comentario  = $form->getValue('comentario');
+                $ativo       = $form->getValue('ativo');
+                $foto          = $form->getValue('foto');
+                $foto1         = $form->getValue('foto1');
+                $locationFile  = $form->foto1->getFileName();
+
+                try 
+                 {
+                        $currentMicroTime = sha1(uniqid(rand(), true));
+                        $nameFile = $currentMicroTime.'.jpg';
+                        //$fullPathNameFile =  '../public/upload/texto/'.$nameFile;
+                        $fullPathNameFile = $this->_file_texto.$nameFile;
+                        $filterRename = new Zend_Filter_File_Rename(
+                                array('target' => $fullPathNameFile, 'overwrite' => true));
+                        $filterRename->filter($locationFile);
+                        $imageAdapter = new Zend_File_Transfer_Adapter_Http();
+                        //$imageAdapter->setDestination('../public/upload/');
+                        $imageAdapter->setDestination( $this->_file_texto );
+                        if( $imageAdapter->receive() )
+                            echo 'Upload efetuado com sucesso';
+                        else
+                        	echo 'Ops! Ocorreu um erro ao enviar o arquivo';
+                 } 
+                    catch (Zend_File_Transfer_Exception $e) {
+                            $e->getMessage();
+                }  
+
+                $this->texto-> add($rotulo, $titulo, $comentario, $nameFile, $ativo);
+
                    if($this->texto)
                     {
-                       $this->_helper->flashMessenger->addMessage(array('successo'=>'Registro Gravado com sucesso'));
+                       $this->_helper->flashMessenger->addMessage(
+                               array('successo'=>'Registro Gravado com sucesso'));
                        $this->_helper->redirector('sucesso');
                     }
             } else {
@@ -96,119 +108,126 @@ class TextoController extends Zend_Controller_Action{
             }
         }
     }
-    public function editarAction(){
+    public function editAction(){
+        
         $this->assecoAction();
+        
         $this->_helper->layout->setLayout('administrator');
-        $form = new Application_Form_Deejay();
+        $form = new Application_Form_Texto();
         $this->view->form = $form;
-	   if ($this->getRequest()->isPost()) 
+        if ($this->getRequest()->isPost()) 
         {
             $formData = $this->getRequest()->getPost();
             if ($form->isValid($formData))
             {
-                $id 	       = (int) $form->getValue('id');
-                $nome 	       = $form->getValue('nome');
-                $endereco      = $form->getValue('endereco');
-                $bibliografia  = $form->getValue('bibliografia');
-                $bairro        = $form->getValue('bairro');
-                $email         = $form->getValue('email');
-                $telefone      = $form->getValue('telefone');
-                $celular       = $form->getValue('celular');			
-                $this->texto->updates($id
-                        , $nome
-                        , $endereco
-                        , $bibliografia
-                        , $bairro
-                        , $email
-                        , $telefone
-                        , $celular
-                        , $nameFile);
+                //$id          = (int) $form->getValue('id');
+                $foto        = $form->getValue('foto');
+                $foto1       = $form->getValue('foto1');
+                $locationFile= $form->foto1->getFileName();
+                $fotoantigo  = $form->getElement('foto')->getValue(); //the hidden field
+                // se nã mudou a foto considetra o mesmo
+                if (file_exists($this->_file_texto.$fotoantigo)) 
+                {
+                    $form->getElement('foto')->setIgnore(true); 
+                    $nomeFoto =  $fotoantigo;
+				} 
+                else
+                { 
+                    // Delete it
+                    $this->removeImages($_POST['foto'], (int)$form->getValue('id') );
+                    $novonomeFoto = sha1(uniqid(rand(), true));
+                    $nomeFoto     = $novonomeFoto.'.jpg';
+                    $caminhoFoto  = $this->_file_texto.$nomeFoto;
+                    // Rename uploaded file using Zend Framework
+                    $filterRename = new Zend_Filter_File_Rename(
+                                    array('target' => $caminhoFoto, 'overwrite' => true));
+                    $filterRename->filter($locationFile);
+                    // detination file
+                    $imageAdapter = new Zend_File_Transfer_Adapter_Http();
+                    $imageAdapter->setDestination( $this->_file_texto );
+                        try 
+                        {
+                            $imageAdapter->receive();
+                        } 
+                        catch (Zend_File_Transfer_Exception $e) 
+                        {
+                            $e->getMessage();
+                        }    
+                }
+                $this->texto-> updates((int) $form->getValue('id')
+				                       , $form->getValue('rotulo')
+				                       , $form->getValue('titulo')
+				                       , $form->getValue('comentario')
+				                       , $nomeFoto
+				                       , $form->getValue('ativo'));
                 if($this->texto)
                     {
-                       $this->_helper->flashMessenger->addMessage(array('successo'=>'Registro alterado com sucesso'));
+                       $this->_helper->flashMessenger->addMessage(
+                               array('successo'=>'Registro alterado com sucesso'));
                        $this->_helper->redirector('sucesso');
                     }
             } else {
                 $form->populate($formData);
             }
-        } else {
+        } 
+        else 
+        {
             $id = $this->_getParam('id', 0);
-            if ($id > 0) {
-                //$deejay = new Application_Model_DbTable_Deejay();
-                $form->populate($this->texto->getId($id));
+            if ($id > 0) 
+            {
+                 $form->populate($this->texto->getId($id));
             }
         }
     }
-    public function excluirAction() {
-        $this->assecoAction();
-        $this->_helper->layout->setLayout('administrator');
-        //$deejay = new Application_Model_DbTable_Deejay();
-        // Recupera o id
-        $id = $this->_request->getParam("id", 0);
-        // Verifica se existe id
-        if($id > 0)
-        {
-            //// Cria o where
-            // Remove o registro
-            $this->texto->delete(array("id = ?" => $id));
-        }
-        // Redireciona
-        //$this->_helper->redirector("index", NULL, NULL);
-        $this->_helper->flashMessenger->addMessage(array('successo'=>'Registro excluido com sucesso'));
-        $this->_helper->redirector('sucesso');
-    }
-    public function deleteAction(){
-        $params = array('host' =>'localhost'
-            , 'username' =>'root'
-            , 'password' =>''
-            , 'dbname'	=>'didi' );
-        $DB = new Zend_Db_Adapter_Pdo_Mysql($params);
-        $request = $this->getRequest();
-        $DB->delete('deejay', 'id = '.$request->getParam('id'));
-        $this->_helper->flashMessenger->addMessage(array('successo'=>'Registro alterado com sucesso'));
-        $this->_helper->redirector('sucesso');
-    }
-    public function viewAction(){
-     $this->assecoAction();
-     $this->_helper->layout->setLayout('administrator');
-     try
-     {
-      $id = $this->_getParam('id', 0);
-      if ($id > 0)
-      {
-         $deejay = new Application_Model_Deejay();
-         $dados = $deejay->select("id = $id", "id", 1);
-         $this->view->assign("dados", $dados);
-       }
-      } 
-      catch (Zend_Db_Exception $exc) {
-         echo $exc->getTraceAsString();
-      }
+    public function deleteAction() {
+    
+    	$this->assecoAction();
+    
+    	$this->_helper->layout->setLayout('administrator');
+    	$id = $this->_request->getParam("id", 0);
+    	if($id > 0)
+    	{
+    		// Delete images
+    		$imageName = $this->texto->getId($id);
+    		$this->removeImages($imageName['foto'],$id);
+    		$this->texto->delete(array("id = ?" => $id));
+    	}
+    
+    	$this->_helper->flashMessenger->addMessage(
+    	        array('successo'=>'Registro excluido com sucesso'));
+    	$this->_helper->redirector('sucesso');
     }
     public function showAction() {
-        $this->assecoAction();
-        $this->_helper->layout->setLayout('administrator');
-        $id = $this->getRequest()->getParam('id');
-        if ($id > 0) 
+    	$this->assecoAction();
+    	$this->_helper->layout->setLayout('administrator');
+    	$id = $this->getRequest()->getParam('id');
+    	if ($id > 0)
     	{
-          $deejay = $this->deejay->find($id)->current(); 
-	  // or $this->Posts->fetchRow("id = $id");
-          $this->view->deejay = $deejay;
-        }
-        else $this->view->message = 'registro não existe';
+    		$dados = $this->texto->find($id)->current();
+    		$this->view->texto = $dados;
+    	}
+    	else $this->view->message = 'registro não existe';
     }
     public function sucessoAction(){
         $this->_helper->layout->setLayout('administrator');
         $dados = "sucesso";
-	   $this->view->assign("dados", $dados);
+	    $this->view->assign("dados", $dados);
     }
     public function assecoAction(){
         // valida sessão
          if (!Zend_Auth::getInstance()->hasIdentity() )
          {
             return $this->_helper->redirector->goToRoute(
-                    array('controller' => 'auth'), null, true);
+                    array('controller' => 'Autenticacao'), null, true);
          }
+    }
+    public function removeImages($img, $id){
+      if (isset($img))
+      {
+    		$imageName = $this->texto->getId($id);
+    		//unlink("../public/upload/".$imageName['foto']);
+    		unlink($this->_file_texto.$imageName['foto']);
+      }
     }
     // region administador
    
---
+++ application/forms/Texto.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Description class Evento
+ *
+ * @author Administrador
+ */
+
+class Application_Form_Texto extends Zend_Form{
+
+    public function init()
+    {
+        $this->setName("Texto");      
+        $this->setAttrib('accept-charset', 'utf-8');     
+	      $this->setAttrib('enctype', 'multipart/form-data');
+        $this->setMethod('post');      
+        $this->addElement('hidden', 'id', '1');
+        $this->addElement('text', 'rotulo', array(
+            'filters'    => array('StringTrim'),
+               'validators' => array(
+                    array(
+                    'validator'=>'NotEmpty',
+                    'options'=>array(
+                            'messages'=>'Rotulo deve ser informado'
+                            ),
+                            'breakChainOnFailure'=>true
+                    ),
+                    array( 'StringLength', false, array(0, 150) ),
+                    ),
+                'required'   => true,
+                //'description' => 'titulo del Evento',
+                'label'      => 'Rotulo:',
+                'class'      => 'input-long'
+        ));
+        $this->addElement('text', 'titulo', array(
+              //'filters'    => array('StringTrim', 'StringToLower'),
+              'filters'    => array('StringTrim'),
+                 'validators' => array(
+                      array(
+                      'validator'=>'NotEmpty',
+                      'options'=>array(
+                              'messages'=>'Titulo deve ser informado'
+                              ),
+                              'breakChainOnFailure'=>true
+                      ),
+                      array( 'StringLength', false, array(0, 150) ),
+                      ),
+                  'required'   => true,
+                  //'description' => 'titulo del Evento',
+                  'label'      => 'Titulo:',
+                  'class'      => 'input-long'
+        ));
+        $this->addElement('textarea', 'comentario', array(
+                 'filters'    => array('StringTrim'),
+                 'validators' => array(
+                      array(
+                      'validator'=>'NotEmpty',
+                      'options'=>array(
+                              'messages'=>'Destaque deve ser informado'
+                              ),
+                              'breakChainOnFailure'=>true
+                      ),
+                      //array('StringLength', false, array(0, 600)),
+                      ),
+                 'required'   => true,
+                 'label'      => 'Destaque:',
+                 'class'      => 'ckeditor'
+                  //'class'      => 'input-long'
+        ));
+        $this->addElement('select', 'ativo', array(
+               'filters'    => array('StringTrim'),
+               'validators' => array(
+                    array(
+                    'validator'=>'NotEmpty',
+                    'options'=>array(
+                            'messages'=>'Status deve ser informado'
+                            ),
+                            'breakChainOnFailure'=>true
+                    ),
+                    array('StringLength', false, array(0, 150)),
+                    ),
+               'required'   => true,
+               'label'      => 'Status:',
+               'class'      => 'input-short',
+               'MultiOptions' =>array('' => '-- Selesionar --',
+                                      'Sim' => 'Sim',
+				      				  'Não' => 'Não')
+        ));
+
+        $this->addElement('hidden', 'foto', '1');
+
+        $foto = new Zend_Form_Element_File('foto1');
+        $foto->setLabel('Foto:')
+                   ->addValidator('Extension', false, array('jpg', 'png', 'gif'))
+                   ->addValidator('Size', false, 1002400)
+                   ->setDescription("Images")
+                   ->setDestination('../public/upload/texto/'); 
+         $this->addElement($foto);
+
+        $this->addElement('submit', 'enviar', array(
+                      'required' => false,
+                      'ignore'   => true,
+                      'class'    => 'submit-green'
+        ));
+        $this->addElement('button', 'Listar', array(
+          'label'    => 'Listar',
+          'required' => false,
+          'ignore'   => true,
+          'order'    => '1',
+          'class'    => 'submit-gray',
+          'attribs'  => array('onclick'=>'List("Texto/index")')
+        ));
+
+        // decorado form com css
+        $this->clearDecorators();
+        $this->addDecorator('FormElements')
+             ->addDecorator('HtmlTag', array('tag' => '<div>','class'=>'module-body'))
+             ->addDecorator('Form');
+
+        $this->setElementDecorators(array(
+                      array('ViewHelper'),
+                      array('Errors'),
+                      array('Description'),
+                      array('Label', array('separator'=>' ')),
+                      array('HtmlTag', array('tag' => 'p')),
+        ));
+
+        $this->getElement('foto1')->setDecorators(
+         array('File','Errors',
+         array(array('td' => 'HtmlTag'), array('tag' => 'p')),
+               array('Label', array('tag' => 'p')),
+               array(array('tr' => 'HtmlTag'), array('tag' => 'p'))
+         ));
+
+        $this->getElement('enviar')->removeDecorator('label')
+            ->setDecorators(array('ViewHelper',
+                            array('HtmlTag',
+                            array('tag'=>'<fieldset>', 'id'=>'element'))) );        
+        $this->getElement('Listar')->removeDecorator('label');
+
+   } //end init form
+}
+?>
